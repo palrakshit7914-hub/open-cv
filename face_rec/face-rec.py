57a97a5c52900ba3f1535e6c75df7108c787379a
Rakshit <palrakshit7914@gmail.com>
2026-06-04 14:21:40 +0530

    Fix face cascade loading and enhance face detection logic

--- face_rec/face-rec.py
+++ face_rec/face-rec.py
@@ -1,8 +1,15 @@
 import cv2
-face_cascade = cv2.cascasdeClassifier('haarcascade_frontalface_default.xml')
+import os 
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 webcam = cv2.VideoCapture(0)
 while True:
     _,img=webcam.read()
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
     cv2.imshow("Face Detection",img)
     key = cv2.waitKey(10)
     if key == 27:
