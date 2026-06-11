ca728d7d0d0eef7dd30b12e197e899e9e6b1a02c
Rakshit <palrakshit7914@gmail.com>
2026-06-11 23:20:57 +0530

    Enhance security camera functionality by adding motion detection and contour visualization

--- security_camera/sec_cam.py
+++ security_camera/sec_cam.py
@@ -7,7 +7,16 @@ while True:
     _,img2 = webcam.read()
     diff = cv2.absdiff(img1,img2)
     gray = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Security Camera",gray)
+    _,thresh = cv2.threshold(gray,20,255,cv2.THRESH_BINARY)
+    cv2.imshow("Security Camera",thresh)
+    countours,_ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    for contour in countours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        winsound.Beep(1000,1000)
+        (x,y,w,h) = cv2.boundingRect(contour)
+        cv2.rectangle(img1,(x,y),(x+w,y+h),(0,255,0),2)
+    
     if cv2.waitKey(10) == 27:
         break
 webcam.release()
