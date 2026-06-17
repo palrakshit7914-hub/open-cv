49a6b07cfb7a1729134956d1611183033e8f05c1
Rakshit <palrakshit7914@gmail.com>
2026-06-17 15:10:20 +0530

    Refactor eye control script to improve camera handling and display functionality

--- eye_control/eye.py
+++ eye_control/eye.py
@@ -3,3 +3,12 @@ import mediaPipe as mp
 import pyautogui
 
 cam = cv2.VideoCapture(0)
+
+while True:
+    _,img = cam.read()
+    cv2.imshow("Eye Control Mouse", img)
+    key = cv2.waitKey(100)
+    if key == 27:
+        break
+cam.release()
+cv2.destroyAllWindows()
