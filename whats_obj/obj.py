38a3870582bc87116b657acb4f1d637769638913
Rakshit <palrakshit7914@gmail.com>
2026-06-25 16:32:16 +0530

    installed ultralytics and open cam

--- whats_obj/obj.py
+++ whats_obj/obj.py
@@ -1,3 +1,13 @@
 import cv2
-import mediapipe as mp
 import os
+from ultralytics import YOLO
+
+
+webcam = cv2.VideoCapture(0)
+while True:
+    _,img = webcam.read()
+    cv2.imshow("Object Detection",img)
+    if cv2.waitKey(10) == 27:
+        break
+webcam.release()
+cv2.destroyAllWindows()
