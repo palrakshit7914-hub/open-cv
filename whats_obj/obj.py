bcc06afe57a71657230d65169f6a822056debdfb
Rakshit <palrakshit7914@gmail.com>
2026-06-26 13:46:16 +0530

    Refactor bounding box coordinate handling and improve confidence value rounding

--- whats_obj/obj.py
+++ whats_obj/obj.py
@@ -13,9 +13,11 @@ while True:
     for r in results:
         img = r.plot()
         for box in r.boxes.xyxy:
-            x1,y1,x2,y2 = r.boxes.xyxy[0]
+            x1,y1,x2,y2 = r.boxes.xyxy[0]#making bounding box coordinates into integers
             x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
-            # conf = r.boxes.conf[0]
+
+            # conf = float(r.boxes.conf[0])
+            conf = round(float(r.boxes.conf[0]), 2)
 
     cv2.imshow("Object Detection",img)
     if cv2.waitKey(10) == 27:
