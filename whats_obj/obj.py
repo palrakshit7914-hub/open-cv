fa6cb09babb14dbd6fbcccb5d2eea1c9663ac4bc
Rakshit <palrakshit7914@gmail.com>
2026-06-26 12:59:03 +0530

    Optimize image mirroring and enhance object detection results display

--- whats_obj/obj.py
+++ whats_obj/obj.py
@@ -7,9 +7,11 @@ model = YOLO("yolov8n.pt")
 webcam = cv2.VideoCapture(0)
 while True:
     _,img = webcam.read()
-    img = cv2.flip(img,0)#mirror the image
+    img = cv2.flip(img,1)#mirror the image
 
     results = model(img,stream=True)#optimized img for webcam
+    for r in results:
+        img = r.plot()
     cv2.imshow("Object Detection",img)
     if cv2.waitKey(10) == 27:
         break
