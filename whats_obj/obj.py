fe9f8910e817ce965ea06064eb679b45ab5e7bcd
Rakshit <palrakshit7914@gmail.com>
2026-06-26 13:04:22 +0530

    refine object detection bounding

--- whats_obj/obj.py
+++ whats_obj/obj.py
@@ -12,6 +12,11 @@ while True:
     results = model(img,stream=True)#optimized img for webcam
     for r in results:
         img = r.plot()
+        for box in r.boxes.xyxy:
+            x1,y1,x2,y2 = r.boxes.xyxy[0]
+            x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
+            # conf = r.boxes.conf[0]
+
     cv2.imshow("Object Detection",img)
     if cv2.waitKey(10) == 27:
         break
